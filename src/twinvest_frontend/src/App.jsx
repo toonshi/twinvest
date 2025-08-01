f3420bad4977b281a3a60f73116ad1decc782b0c
deborah <fabbydebby@gmail.com>
2025-08-01 20:30:11 +0300

    changes

--- src/twinvest_frontend/src/App.jsx
+++ src/twinvest_frontend/src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from "react";
 import { twinvest_backend } from "declarations/twinvest_backend";
-import Header from './sections/Header'
-import Hero from "./sections/Hero";
-import About from "./sections/About";
+
 
 function App() {
   return (
     <main>
-      <Header />
-      <Hero />
-      <About />
+     
     </main>
   );
 }
