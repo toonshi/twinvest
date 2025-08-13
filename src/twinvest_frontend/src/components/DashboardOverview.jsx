43e9a82e867db17de122c5bb43bf0707029d2344
deborah <fabbydebby@gmail.com>
2025-08-13 12:53:09 +0300

    SME/Freelancer-dashboard

--- src/twinvest_frontend/src/components/DashboardOverview.jsx
+++ src/twinvest_frontend/src/components/DashboardOverview.jsx
@@ -1,6 +1,6 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
-
+import React from "react";
 import { Button } from "@/components/ui/button";
 import { 
   DollarSign, 
